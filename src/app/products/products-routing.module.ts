2a66a0bb8d4ff9dcadb84957986b40962d3717d3
Matheus Marques <matheusmarques312@gmail.com>
2020-12-05 01:10:58 -0300

    Criando telas de listagem, visualizacao, criacao e edicao

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CartComponent } from './cart/cart.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductViewComponent } from './product-view/product-view.component';
 import { ProductsComponent } from './products.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: ProductsComponent },
   { path: ':id', pathMatch: 'full', component: ProductViewComponent },
-  { path: 'cart', pathMatch: 'full', component: CartComponent },
+  { path: 'cart', pathMatch: 'full', component: ProductDetailsComponent },
 ];
 
 @NgModule({
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { CartComponent } from './cart/cart.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductManagementComponent } from './product-management/product-management.component';
+import { ProductRegistrationComponent } from './product-registration/product-registration.component';
 import { ProductViewComponent } from './product-view/product-view.component';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
 
 @NgModule({
-  declarations: [ProductsComponent, ProductItemComponent, ProductViewComponent, CartComponent],
+  declarations: [
+    ProductsComponent,
+    ProductItemComponent,
+    ProductViewComponent,
+    CartComponent,
+    ProductManagementComponent,
+    ProductRegistrationComponent,
+    ProductDetailsComponent,
+  ],
   imports: [ProductsRoutingModule, SharedModule],
   providers: [],
 })
